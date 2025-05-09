3518cf6a9b8a577486b3e97f8234faaa21d43664
zvictorliu <zvictorliui@outlook.com>
2025-05-09 13:59:25 +0800

    style: vue-cli to vite
    - the vite style

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomePage from '@/views/HomePage.vue'; // 引入主页组件
-import MovieDetailPage from '@/views/MovieDetail.vue'; // 引入影片详情页组件
-import ActorDetailPage from '@/views/ActorDetail.vue'; // 引入演员详情页组件
+import HomePage from '../views/HomePage.vue'; // 引入主页组件
+import MovieDetailPage from '../views/MovieDetail.vue'; // 引入影片详情页组件
+import ActorDetailPage from '../views/ActorDetail.vue'; // 引入演员详情页组件
 
 const routes = [
   {
