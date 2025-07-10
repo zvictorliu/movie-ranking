bab07075b35ce00510d6c651cb2186b52a27ae13
zvictorliu <zvictorliui@outlook.com>
2025-07-10 16:58:08 +0800

    build: docker support
    - 部署到docker

--- backend/app.py
+++ backend/app.py
@@ -377,4 +377,4 @@ def update_movie(id):
     return jsonify({"success": True, "message": "影片信息更新成功"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000, host="0.0.0.0")
